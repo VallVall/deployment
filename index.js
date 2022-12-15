0c96c7c3e1f68af2fd6e6d5fb260eecfb3d9cc40
Valentin Stratovich <valikstratovich@gmail.com>
2022-12-15 14:17:38 +0200

    change index.js placement

--- index.js
+++
@@ -1,6 +0,0 @@
-import { sum } from './src/utils/sum.js';
-import { dublicate } from './src/utils/dublicate.js';
-
-console.log(sum(1, 2));
-
-console.log(dublicate('hello'));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { sum } from './utils/sum.js';
+import { dublicate } from './utils/dublicate.js';
+
+console.log(sum(1, 2));
+
+console.log(dublicate('hello'));

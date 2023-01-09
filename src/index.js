731310b448c119e0764f97fe736a97b4cc9db8bf
Valentin Stratovich <valikstratovich@gmail.com>
2023-01-09 14:08:34 +0200

    clean clogs

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@
 import { sum } from './utils/sum.js';
 import { dublicate } from './utils/dublicate.js';
 
-console.log('process.env.MODE', process.env.MODE);
-
 console.log(sum(1, 2));
 
 console.log(dublicate('hello'));
-
-console.log('process.env.MODE', process.env.VALIK);
 
 const paragraph = document.createElement('p');
 
